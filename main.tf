346f9830119d3e8206bd3327594da5a5afe3202a
pratishthavermattn <pratishtha.verma@tothenew.com>
2021-06-15 16:56:14 +0530

    Drupal Updated

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 module "db" {
   source        = "./modules/db/"
-  vpc_asg       = module.network.vpc_id_all
-  sec_group_rds = module.network.security_group_id_rds
-  subnet_rds    = module.network.public_sn_asg
+  vpc_asg       = var.vpc_drupal
+  sec_group_rds = var.sec_group_drupal_rds
+  subnet_rds    = var.subnet_drupal_rds
 }
 
 module "asg" {
-  source        = "./modules/asg/"
-  subnet_asg    = module.network.public_sn_asg
-  sec_group_asg = module.network.security_group_id_asg
+  source = "./modules/asg/"
+  #subnet_asg    = var.demo
+  subnet_asg    = var.subnet_drupal_asg
+  sec_group_asg = var.sec_group_drupal_asg
   rds_point     = module.db.rds_endpoint
   depends_on    = [module.db.rds_endpoint]
   target_gp     = module.alb.tg
@@ -24,7 +25,7 @@ module "alb" {
 
 module "efs" {
   source        = "./modules/efs/"
-  subnet_efs    = module.network.private_sn_asg
-  sec_group_efs = module.network.security_group_id_asg
-  vpc_efs       = module.network.vpc_id_all
+  subnet_efs    = var.subnet_drupal_efs
+  sec_group_efs = var.sec_group_drupal_efs
+  vpc_efs       = var.vpc_drupal_efs
 }
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "vpc_drupal" {
+
+}
+
+variable "sec_group_drupal_rds" {
+
+}
+
+variable "subnet_drupal_rds" {
+
+}
+
+variable "subnet_drupal_asg" {
+
+}
+
+variable "sec_group_drupal_asg" {
+
+}
+
+variable "subnet_drupal_efs" {
+
+}
+
+variable "sec_group_drupal_efs" {
+
+}
+
+variable "vpc_drupal_efs" {
+
+}
